bfd78f4b2026b65817ede9fce3caecff69cb089f
ismaelardo <ismaelardo@gmail.com>
2022-01-27 02:55:35 -0300

    agregué STAGE en cada stage para que no se confunda el failure

--- maven.groovy
+++ maven.groovy
@@ -7,19 +7,24 @@
 def call(){
   
   	stage('Compile') {
-            sh './mvnw clean compile -e'
-            sh 'exit 1'
+  		STAGE = env.STAGE_NAME
+        sh './mvnw clean compile -e'
+        sh 'exit 1'
     }
     stage('Test') {
+    	STAGE = env.STAGE_NAME
         sh './mvnw clean test -e'
     }
     stage('Jar') {
+    	STAGE = env.STAGE_NAME
         sh './mvnw clean package -e'
     }
     stage('Run') {
+    	STAGE = env.STAGE_NAME
         sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &'
     }
     stage('Testing') {
+    	STAGE = env.STAGE_NAME
         sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
 }
